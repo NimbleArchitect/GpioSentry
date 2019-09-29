ce92e3b148c0d76f8429d5703391dacf1b456399
Richard Davis <richarddavis04@yahoo.co.uk>
2019-09-29 18:28:02 +0100

    forgot to add gpiopins.rs file

---
+++ src/gpiopins.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+use crate::conf;
+
+
+pub struct PinState {
+    pub state: i32,
+    pub prev_state: i32,
+    pub timer: i32,
+}
+
+impl Default for PinState {
+    fn default () -> PinState {
+        PinState{
+            state: 0, prev_state: 0, timer: 0
+        }
+    }
+}
+
+
+pub fn init_pins(gpio: &rppal::gpio::Gpio , pindata:&HashMap<String, conf::PinConfig>) -> HashMap<u8, PinState>{
+
+    let mut new_pins = HashMap::new();
+
+    //let contact_pin = gpio.get(CONTACT_PIN)?.into_input();
+    for (_id, info) in pindata {
+        let pinnum = info.pin;
+        let exists = new_pins.contains_key(&pinnum);
+        if exists == false {
+            let pin_state = PinState::default();
+            let result_pin = gpio.get(pinnum);
+            
+            let this_pin = match result_pin {
+                Ok(this_pin) => this_pin,
+                Err(_) => panic!("invalid gpio number!"),
+            };
+            let mut input_pin = this_pin.into_input();
+            input_pin.set_reset_on_drop(false);
+            
+            new_pins.insert(pinnum, pin_state);
+        }
+    }
+    new_pins
+}
