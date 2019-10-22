628f938e3b4de33c55bbe09ec744d31fb0e944e9
NimbleArchitect <56797391+NimbleArchitect@users.noreply.github.com>
2019-10-23 00:26:55 +0100

    Added license header

--- src/conf.rs
+++ src/conf.rs
@@ -1,8 +1,19 @@
-/// GpioSentry
+/// GpioSentry - Reads the comfiguration file and spits out a hashmap of the PinConfig struct
 ///
-/// Created by: NimbleArchitect
+///    Copyright (C) 2019 NimbleArchitect
 ///
-/// Reads the compiguration file and spits out a hashmap of the PinConfig struct
+///    This program is free software: you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation, either version 3 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///    but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ///
 
 use std::collections::HashMap;
